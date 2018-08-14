ecbf0dea445f71f4e727344b6c9189af43d3607a
xiaokukuo <976886537@qq.com>
2018-08-14 16:34:34 +0800

    springTest

--- springtest/src/main/java/com/springmybatis/App.java
+++ springtest/src/main/java/com/springmybatis/App.java
@@ -1,13 +1,14 @@
 package com.springmybatis;
 
+import org.mybatis.spring.SqlSessionFactoryBean;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+
+        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        System.out.println("Hello World!" );
     }
 }
